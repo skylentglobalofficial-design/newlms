61d2fd33fb28b29a03ac2b2e962eac18aff246f7
Skylent Global <skylentglobalofficial-design@users.noreply.github.com>
2026-09-23 15:15:00 +0530

    Refine homepage Phase 4 responsive visual system

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,11 @@
-import { PageShell } from "../components/shared"
+﻿import { PageShell } from "../components/shared"
 import SkylentHeroLearningModel from "../components/home/SkylentHeroLearningModel"
 import SkylentHomeGoals from "../components/home/SkylentHomeGoals"
-import SkylentHomeCatalogue from "../components/home/SkylentHomeCatalogue"
 import SkylentHomeEvidence from "../components/home/SkylentHomeEvidence"
+import SkylentHomeCatalogue from "../components/home/SkylentHomeCatalogue"
 import SkylentHomeCareerOS from "../components/home/SkylentHomeCareerOS"
-import SkylentHomeIdentity from "../components/home/SkylentHomeIdentity"
-import SkylentHomeProgrammeDepth from "../components/home/SkylentHomeProgrammeDepth"
-import SkylentHomePath from "../components/home/SkylentHomePath"
-import SkylentHomeProjects from "../components/home/SkylentHomeProjects"
-import SkylentHomeLabs from "../components/home/SkylentHomeLabs"
 import SkylentHomeEducation from "../components/home/SkylentHomeEducation"
-import SkylentHomeSchool from "../components/home/SkylentHomeSchool"
-import SkylentHomeUniversity from "../components/home/SkylentHomeUniversity"
-import SkylentHomeExams from "../components/home/SkylentHomeExams"
-import SkylentHomeWorkshops from "../components/home/SkylentHomeWorkshops"
 import SkylentHomeOSArchitecture from "../components/home/SkylentHomeOSArchitecture"
-import SkylentHomeJourney from "../components/home/SkylentHomeJourney"
-import SkylentHomeUniverse from "../components/home/SkylentHomeUniverse"
-import SkylentHomeStart from "../components/home/SkylentHomeStart"
 import SkylentHomeFinal from "../components/home/SkylentHomeFinal"
 import "./HomePage.css"
 import "../components/home/SkylentHomeChapters.css"
@@ -26,25 +14,28 @@ export default function HomePage() {
   return (
     <PageShell aurora={false}>
       <div className="home-p3">
+        {/* 01 — What Skylent is */}
         <SkylentHeroLearningModel />
+
+        {/* 02 â€” Start with a goal */}
         <SkylentHomeGoals />
-        <SkylentHomeCatalogue />
+
+        {/* 03 â€” How Skylent works */}
         <SkylentHomeEvidence />
+
+        {/* 04 â€” Learning ecosystem */}
+        <SkylentHomeCatalogue />
+
+        {/* 05 â€” Career OS */}
         <SkylentHomeCareerOS />
-        <SkylentHomeIdentity />
-        <SkylentHomeProgrammeDepth />
-        <SkylentHomePath />
-        <SkylentHomeProjects />
-        <SkylentHomeLabs />
+
+        {/* 06 â€” Education */}
         <SkylentHomeEducation />
-        <SkylentHomeSchool />
-        <SkylentHomeUniversity />
-        <SkylentHomeExams />
-        <SkylentHomeWorkshops />
+
+        {/* 07 â€” The Skylent OS */}
         <SkylentHomeOSArchitecture />
-        <SkylentHomeJourney />
-        <SkylentHomeUniverse />
-        <SkylentHomeStart />
+
+        {/* 08 â€” Final CTA */}
         <SkylentHomeFinal />
       </div>
     </PageShell>
